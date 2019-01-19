46c727dff2f1730d5fdd41a0c63fdf52e9f9a3c4
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-19 17:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   array = []
-  for (i = 0, i < musicians.length + 1, i++) {
+  for (i = 0, i < musicians.length, i++) {
     /*array.push(`${musicians[i]} plays ${instruments[i]}`)*/
   }
   return array
